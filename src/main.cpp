e5d008f1c83259409d6ec041d74204bb3f12c979
Kyungbin Min <kbmin2711@gmail.com>
2025-07-25 22:30:13 +0900

    Fix RST pin definition and update setup sequence for RFM69HCW module

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,11 @@
 // Pin 11 (MOSI)   -> MOSI
 // Pin 10 (CS)     -> CS  (Configurable below)
 // Pin 2  (IRQ)    -> G0  (Configurable below)
-// Pin 9  (RST)    -> RST (Configurable below)
+// Pin 3  (RST)    -> RST (Configurable below)
 
 #include <Arduino.h>
-#include <SPI.h>
 #include <RH_RF69.h>
+#include <SPI.h>
 
 // #################### CONFIGURATION ####################
 //
@@ -38,21 +38,17 @@
 // STEP 3:
 // --------
 // Define the pins used for CS, IRQ, and RST.
-#define RFM69_CS      10
-#define RFM69_IRQ     digitalPinToInterrupt(2)
-#define RFM69_RST     9
+#define RFM69_CS 10
+#define RFM69_IRQ digitalPinToInterrupt(2)
+#define RFM69_RST 3
 // #######################################################
-
 
 // Define Node Addresses
 #ifdef IS_NODE_1
-  #define MY_ADDRESS      1   // This node's address
-  #define DEST_ADDRESS    2   // The destination node's address
+#define MY_ADDRESS 1 // This node's address
 #else
-  #define MY_ADDRESS      2   // This node's address
-  #define DEST_ADDRESS    1   // The destination node's address
+#define MY_ADDRESS 2 // This node's address
 #endif
-
 
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_IRQ);
@@ -61,51 +57,38 @@ RH_RF69 rf69(RFM69_CS, RFM69_IRQ);
 long lastSendTime = 0;
 
 void setup() {
-  Serial.begin(115200);
-  // Wait for the serial port to be ready (especially for native USB devices like Teensy)
-  while (!Serial) {
-    delay(10);
-  }
+  Serial.begin(9600);
 
-  Serial.println("RFM69HCW Test Sketch");
-  Serial.print("Node #"); Serial.println(MY_ADDRESS);
-
-  // Manual reset of the radio module
+  // For Teensy 3.x and T4.x the following format is required to operate correctly
+  // pinMode(LED, OUTPUT);
   pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+
+  Serial.println("Teensy RFM69 Client!");
+  Serial.println();
+
+  // manual reset
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
+  //----- END TEENSY CONFIG
 
-  // Initialize the radio driver
-  if (!rf69.init()) {
-    Serial.println("RFM69 radio init failed!");
-    while (1); // Halt execution
-  }
-  Serial.println("RFM69 radio init OK!");
-
-  // Set the frequency
-  if (!rf69.setFrequency(RF69_FREQ)) {
+  if (!rf69.init())
+    Serial.println("init failed");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  // No encryption
+  if (!rf69.setFrequency(433.0))
     Serial.println("setFrequency failed");
-  }
-  Serial.print("Frequency set to: "); Serial.println(RF69_FREQ);
 
-  // Set the node addresses
-  rf69.setThisAddress(MY_ADDRESS);
-  rf69.setHeaderTo(DEST_ADDRESS);
+  // If you are using a high power RF69, you *must* set a Tx power in the
+  // range 14 to 20 like this:
+  rf69.setTxPower(14);
 
-  // // Set an encryption key (16 bytes).
-  // // IT'S IMPORTANT THAT BOTH NODES USE THE SAME KEY!
-  // uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-  //                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  // rf69.setEncryptionKey(key);
-
-  // Set the transmit power.
-  // For RFM69HCW, you can go up to 20dBm. The 'true' flag enables the high power PA.
-  rf69.setTxPower(10, true);
-
-  Serial.println("Setup complete. Starting loop...");
-  Serial.println("---------------------------------");
+  // The encryption key has to be the same as the one in the second board
+  uint8_t key[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  rf69.setEncryptionKey(key);
 }
 
 void loop() {
@@ -122,7 +105,7 @@ void loop() {
       Serial.print("Received [");
       Serial.print(len);
       Serial.print(" bytes]: ");
-      Serial.print((char*)buf);
+      Serial.print((char *)buf);
       Serial.print(" | RSSI: ");
       Serial.println(rf69.lastRssi(), DEC);
 
